8ea17afa93c14a728895fa7d30b85eb2fe4f197b
LeavesBreathe <learn4everandever@yahoo.com>
2016-06-21 16:45:25 -0400

    added tensorarray class for testing tensorarrays do allow the while loop to function currently having errors on timestep 2

--- src/AdaptiveComputationTime.py
+++ src/AdaptiveComputationTime.py
@@ -2,7 +2,7 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-import pickle
+import pickle, os, sys
 from datetime import datetime
 from collections import defaultdict
 import tensorflow as tf
@@ -11,8 +11,9 @@ from config import SmallConfig
 try:
     from tensorflow.models.rnn import rnn_cell, rnn, seq2seq
 except:
-    rnn_cell = tf.nn.rnn_cell
-    rnn = tf.nn.rnn
+    sys.path.insert(0, os.environ['HOME'])
+    from Project_RNN_Enhancement.rnn_enhancement import rnn_cell_enhanced as rnn_cell
+    from Project_RNN_Enhancement.rnn_enhancement import rnn_enhanced as rnn
     seq2seq = tf.nn.seq2seq
 from tensorflow.python.ops import array_ops, functional_ops
 from tensorflow.python.ops import control_flow_ops
@@ -49,18 +50,14 @@ class ACTModel(object):
 
         with tf.variable_scope("ACT"):
 
-            act = ACTCell(self.config.hidden_size, inner_cell,0.01,100)
+            act = ACTCell_TensorArray(self.config.hidden_size, inner_cell, 0.01, 10)
 
         embedding = tf.get_variable('embedding', [self.config.vocab_size, self.config.hidden_size])
         inputs = tf.nn.embedding_lookup(embedding, self.input_data)
         inputs = [tf.squeeze(single_input, [1]) for single_input in tf.split(1, self.config.num_steps, inputs)]
         #inputs = tf.pack(inputs)
 
-        try:
-            self.outputs, final_state = rnn.rnn(act,inputs,initial_state=rnn_state
-                                            ,sequence_length=self.num_steps)
-        except:
-            self.outputs, final_state = rnn(act,inputs,initial_state=rnn_state
+        self.outputs, final_state = rnn.rnn(act, inputs,initial_state=rnn_state
                                             ,sequence_length=self.num_steps)
 
         ###### Softmax to get distribution over vocab #######
@@ -116,7 +113,7 @@ class ACTCell(rnn_cell.RNNCell):
     def state_size(self):
         return self._num_units
 
-    def __call__(self, inputs, state, scope=None):
+    def __call__(self, inputs, state, timestep = 0, scope=None):
 
         with vs.variable_scope(scope or type(self).__name__):
 
@@ -131,6 +128,7 @@ class ACTCell(rnn_cell.RNNCell):
                 tf.logical_and(tf.less(prob,self.one_minus_eps), tf.less(counter,self.N))
 
             _,iterations,_,_,acc_outputs,acc_states,acc_probs = control_flow_ops.while_loop(
+
                 pred,self.ACTStep,[prob,counter,state,inputs, acc_outputs, acc_states, acc_probs])
 
 
@@ -169,7 +167,7 @@ class ACTCell(rnn_cell.RNNCell):
 
         output, new_state = rnn.rnn(self.cell, [input], state, scope=type(self.cell).__name__)
 
-        prob_w = tf.get_variable("prob_w", [self.cell.input_size,1])
+        prob_w = tf.get_variable("prob_w", [self.cell.state_size,1]) #corrected to state_size
         prob_b = tf.get_variable("prob_b", [1])
         p = tf.squeeze(tf.nn.sigmoid(tf.matmul(new_state,prob_w) + prob_b))
 
@@ -187,3 +185,98 @@ class ACTCell(rnn_cell.RNNCell):
         return tf.reduce_sum(n_iterations + remainder)
 
 
+class ACTCell_TensorArray(rnn_cell.RNNCell):
+
+    """An RNN cell implementing Graves' Adaptive Computation Time algorithm"""
+
+
+    def __init__(self, num_units, cell, epsilon, max_computation):
+
+        self.one_minus_eps = tf.constant(1.0 - epsilon)
+        self._num_units = num_units
+        self.cell = cell
+        self.N = tf.constant(max_computation)
+
+    @property
+    def input_size(self):
+        return self._num_units
+    @property
+    def output_size(self):
+        return self._num_units
+    @property
+    def state_size(self):
+        return self._num_units
+
+    def __call__(self, inputs, state, timestep = 0, scope=None):
+
+        print('inputs', inputs)
+        print('state', state)
+
+
+        with vs.variable_scope(scope or type(self).__name__):
+
+            # define within cell constants/ counters used to control while loop
+            prob = tf.constant(0.0,tf.float32, name="prob")
+            counter = tf.constant(0, dtype=tf.int32, name="counter")
+            acc_probs, acc_outputs, acc_states = [],[],[]
+
+
+            #dynamic set to true because we don't know how many N iterations will occur
+            acc_probs = TensorArray(dtype = tf.float32, size = 1, dynamic_size = True, clear_after_read = False,
+                tensor_array_name = "accumulated_probabilities", infer_shape = True)
+            acc_outputs = TensorArray(dtype = tf.float32, size = 1, dynamic_size = True, clear_after_read = False,
+                tensor_array_name = "accumulated_outputs", infer_shape = True)
+            acc_states = TensorArray(dtype = tf.float32, size = 1, dynamic_size = True, clear_after_read = False,
+                tensor_array_name = "accumulated_states", infer_shape = True)             
+
+            # the predicate for stopping the while loop. Tensorflow demands that we have
+            # all of the variables used in the while loop in the predicate.
+            pred = lambda prob,counter,state,input,acc_output,acc_state,acc_probs:\
+                tf.logical_and(tf.less(prob,self.one_minus_eps), tf.less(counter,self.N))
+
+            _,iterations,_,_,acc_outputs,acc_states,acc_probs = control_flow_ops.while_loop(
+                
+                pred,self.ACTStep,[prob,counter,state,inputs, acc_outputs, acc_states, acc_probs])
+
+        #nick as a reminder, you can pack the tensorarray to convert it to a regular tensor
+        remainder = 1.0 - acc_probs.read(iterations)
+        acc_probs.write(iterations, remainder) #not sure if this command is correct
+
+        acc_probs = acc_probs.pack()
+        acc_states = acc_states.pack()
+        acc_outputs = acc_outputs.pack()
+
+        next_state = tf.reduce_sum(acc_probs * acc_states, reduction_indices = 0) #check reduction indices
+        output = tf.reduce_sum(acc_probs * acc_outputs, reduction_indices = 0)
+
+        tf.add_to_collection("ACT_remainder", remainder)
+        tf.add_to_collection("ACT_iterations", iterations)
+        print('got through one complete timestep')
+
+        print('output', output)
+        print('next_state', next_state)
+
+        return output, next_state
+
+    def ACTStep(self,prob,counter,state,input,acc_outputs,acc_states,acc_probs):
+
+        #you may need to change this to rnn.rnn depending on tensorflow versions
+        output, new_state = rnn.rnn(self.cell, [input], state, scope=type(self.cell).__name__)
+
+        with tf.variable_scope('sigmoid_activation_for_pondering'):
+            # prob_w = tf.get_variable("prob_w", [self.cell.state_size,1]) #corrected to state_size
+            # prob_b = tf.get_variable("prob_b", [1])
+            # p = tf.squeeze(tf.nn.sigmoid(tf.matmul(new_state,prob_w) + prob_b))
+            p = tf.nn.rnn_cell._linear(new_state[0], 1, True)
+ 
+        acc_outputs.write(counter, output[0])
+        acc_states.write(counter, new_state[0])
+        acc_probs.write(counter, p)
+
+        return [prob + p,counter + 1,new_state[0], input, acc_outputs,acc_states,acc_probs]
+
+    def get_ponder_cost(self, epsilon):
+
+        n_iterations = tf.get_collection_ref("ACT_iterations")
+        remainder = tf.get_collection_ref("ACT_remainder")
+        return tf.reduce_sum(n_iterations + remainder)
