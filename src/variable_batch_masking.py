1b5a0337b138963c4bcb1256b08930377c9d6a20
LeavesBreathe <learn4everandever@yahoo.com>
2016-06-24 20:03:39 -0400

    integrated mlp into halting prob calculation

--- src/variable_batch_masking.py
+++ src/variable_batch_masking.py
@@ -14,13 +14,16 @@ class ACTCellMasking(rnn_cell.RNNCell):
     """An RNN cell implementing Graves' Adaptive Computation Time algorithm"""
 
 
-    def __init__(self, num_units, cell, epsilon, max_computation, batch_size, use_lstm = False):
+    def __init__(self, num_units, cell, epsilon, max_computation, batch_size, use_lstm = False,
+        use_mlp_for_halting_probability = False, num_perceptron_layers = 2):
 
         self.batch_size = batch_size
         self.one_minus_eps = tf.constant(1.0 - epsilon, tf.float32,[self.batch_size])
         self._num_units = num_units
         self.cell = cell
         self.use_lstm = use_lstm
+        self.use_mlp_for_halting_probability = use_mlp_for_halting_probability
+        self.num_perceptron_layers = num_perceptron_layers
         self.N = tf.constant(max_computation, tf.float32,[self.batch_size])
         self.ACT_remainder = []
         self.ACT_iterations = []
@@ -59,8 +62,8 @@ class ACTCellMasking(rnn_cell.RNNCell):
 
 
         '''Calculate ponder cost parts. Reduce mean is used to normalize cost by the batch size'''
-        # self.ACT_remainder.append(tf.reduce_mean(1 - prob)) #TODO: double check this
-        # self.ACT_iterations.append(tf.reduce_mean(iterations))
+        self.ACT_remainder.append(tf.reduce_mean(1 - prob)) #TODO: double check this
+        self.ACT_iterations.append(tf.reduce_mean(iterations))
 
         return output, next_state
 
@@ -71,9 +74,26 @@ class ACTCellMasking(rnn_cell.RNNCell):
             tf.add_n(self.ACT_remainder)/len(self.ACT_remainder) +
             tf.to_float(tf.add_n(self.ACT_iterations)/len(self.ACT_iterations)))
 
+
+    def CalculateHaltingProbability(self, input_for_eval_halting_p):
+        #TODO: leavesbreathe, determine if leaky relu is better. Also integrate batch norm here?
+        def LeakyRELU(input, alpha = 0.01):
+            return tf.maximum(alpha*input, input)
+
+        with tf.variable_scope('sigmoid_activation_for_pondering'): #start with a bias of 1.0
+            if self.use_mlp_for_halting_probability:
+                for layer in xrange(self.num_perceptron_layers-1):
+                    with tf.variable_scope('mlp_layer_'+str(layer)):
+                        input_for_eval_halting_p = tf.nn.relu(tf.nn.rnn_cell._linear(input_for_eval_halting_p, self.output_size, True))
+                with tf.variable_scope('mlp_second_layer'):
+                    p = tf.squeeze(tf.sigmoid(tf.nn.rnn_cell._linear(first_layer_mlp, 1, True, 1.0)))
+            else:
+                p = tf.squeeze(tf.sigmoid(tf.nn.rnn_cell._linear(input_for_eval_halting_p, 1, True, 1.0)))
+        return p
+
     def ACTStep(self,batch_mask,prob_compare,prob,counter,state,input,acc_outputs,acc_states):
 
-        # input is now [batch_size, hidden_size]
+        #TODO: leavesbreathe, implement batch norming hidden state?
         output, new_state = rnn(self.cell, [input], state, scope=type(self.cell).__name__)
 
         
@@ -81,9 +101,7 @@ class ACTCellMasking(rnn_cell.RNNCell):
             input_for_eval_halting_p, _ = tf.split(1,2,new_state)
         else:
             input_for_eval_halting_p = new_state
-
-        with tf.variable_scope('sigmoid_activation_for_pondering'):
-            p = tf.squeeze(tf.sigmoid(tf.nn.rnn_cell._linear(input_for_eval_halting_p, 1, True)))
+        p = self.CalculateHaltingProbability(input_for_eval_halting_p)
 
         # here we create a mask on the p vector, which we then multiply with the state/output.
         # if p[i] = 0, then we have passed the remainder point for that example, so we multiply
@@ -133,9 +151,7 @@ class ACTCellMasking(rnn_cell.RNNCell):
 
         acc_state, acc_output = tf.cond(condition, normal, use_remainder)
 
-        #counter = tf.Print(counter, [counter], message = 'this is the counter before adding one: ', summarize = 10)
-
-
+ 
         # only increment the counter for the examples which are still running
         # counter += tf.constant(1.0,tf.float32,[self.batch_size])
         return [new_batch_mask,prob_compare,prob,counter,new_state, input, acc_output,acc_state]
